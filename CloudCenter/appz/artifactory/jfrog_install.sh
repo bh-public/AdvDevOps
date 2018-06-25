9104b30b63f4f849dedefb9b221a1d2c05b0dc9b
bh-public <bh.public@comcast.net>
2018-06-25 15:16:07 -0700

    fixed http git path in jfrog_install.sh script to point to right place

--- CloudCenter/appz/artifactory/jfrog_install.sh
+++ CloudCenter/appz/artifactory/jfrog_install.sh
@@ -6,21 +6,24 @@ exec > >(tee -a /var/tmp/jfrog-node-init_$$.log) 2>&1
 
 
 #Declaring variable used in the script
-LOCAL_REPO="http://192.168.130.206"
+# Removing local repo, to point to bh-public github
+#LOCAL_REPO="http://192.168.130.206"
 
 # In case you have a local CloudCenter repository, disable it
 sudo sed -i "s/enabled=1/enabled=0/" /etc/yum.repos.d/cliqr.repo 
 
 # get the jfrog repository from the local repo as we have some issues with the Cisco proxy
 cd /etc/yum.repos.d/
-sudo wget $LOCAL_REPO/appz/artifactory/jfrog.repo
+#sudo wget $LOCAL_REPO/appz/artifactory/jfrog.repo
+# Modified for github
+https://raw.githubusercontent.com/bh-public/AdvDevOps/master/CloudCenter/appz/artifactory/jfrog.repo
  
 agentSendLogMessage "Installing jFrog Artifactory..."
 sudo yum install jfrog-artifactory-oss -y
 
 # Checking if Java exist as it's a pre-requisite of tomcat
 if [ -n `command -v java` ]; then
-  agentSendLogMessage "Java check fails: installing JAVA..."
+  agentSendLogMessage "Java install check failed: we are now installing JAVA..."
   sudo yum install java -y
 else
   agentSendLogMessage "Artifactory pre-requisuite checks ok"
